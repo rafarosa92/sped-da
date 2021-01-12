4779dd53c157a076944033cdf8a1d07df7601613
Rafael Rosa - Sidedoor { } <rafael@sidedoor.com.br>
2021-01-12 11:10:32 -0300

    finish php

--- api/nfe/danfe.php
+++ api/nfe/danfe.php
@@ -5,14 +5,21 @@ require_once '../../bootstrap.php';
 
 use NFePHP\DA\NFe\Danfe;
 
-$xml = file_get_contents(__DIR__ . '/fixtures/mod55-nfe.xml');
-$logo = 'data://text/plain;base64,'. base64_encode(file_get_contents(realpath(__DIR__ . '/../images/tulipas.png')));
-//$logo = realpath(__DIR__ . '/../images/tulipas.png');
+$xml = file_get_contents('php://input');
 
 try {
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+
+        throw new Exception('Método incorreto!');
+    }
+    if (empty($xml)) {
+        throw new Exception('Você deve enviar um XML de um NF-e');
+    }
     $danfe = new Danfe($xml);
     $danfe->debugMode(false);
-    $danfe->creditsIntegratorFooter('WEBNFe Sistemas - http://www.webenf.com.br');
+    $danfe->creditsIntegratorFooter('| Sidedoor { } - https://sidedoor.com.br');
+    $danfe->setDefaultFont('times');
+    $danfe->setDefaultDecimalPlaces(2);
     // Caso queira mudar a configuracao padrao de impressao
     /*  $this->printParameters( $orientacao = '', $papel = 'A4', $margSup = 2, $margEsq = 2 ); */
     //Informe o numero DPEC
@@ -21,8 +28,8 @@ try {
     /*  $danfe->logoParameters($logo, 'C', false);  */
     //Gera o PDF
     $pdf = $danfe->render($logo);
-    header('Content-Type: application/pdf');
-    echo $pdf;
+    header('Content-Type: application/pdf;base64; charset=UTF-8');
+    echo  base64_encode($pdf);
 } catch (InvalidArgumentException $e) {
     echo "Ocorreu um erro durante o processamento :" . $e->getMessage();
-}    
+}
